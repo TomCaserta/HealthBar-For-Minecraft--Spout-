bc7fdf9c1b4d4dddac37d43fb2ab1f749bf3bbec
ManiacTwister <ManiacTwister@s7t.de>
2012-02-25 21:18:33 +0100

    Updatet to new Event System, Configuration, and replaced Spout deprecated code

--- src/me/plornt/healthbar/HealthBarHeroes.java
+++ src/me/plornt/healthbar/HealthBarHeroes.java
@@ -3,7 +3,7 @@ package me.plornt.healthbar;
 import org.bukkit.plugin.Plugin;
 
 import com.herocraftonline.dev.heroes.Heroes;
-import com.herocraftonline.dev.heroes.persistence.HeroManager;
+import com.herocraftonline.dev.heroes.hero.HeroManager;
 
 public class HealthBarHeroes {
     static HeroManager pl;
--- src/me/plornt/healthbar/HealthBarPluginListener.java
+++ src/me/plornt/healthbar/HealthBarPluginListener.java
@@ -1,16 +1,19 @@
 package me.plornt.healthbar;
 
 import org.bukkit.event.server.PluginEnableEvent;
-import org.bukkit.event.server.ServerListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 
-public class HealthBarPluginListener extends ServerListener {
+public class HealthBarPluginListener implements Listener {
     private HealthBar pl;
 
     public HealthBarPluginListener(HealthBar plugin) {
         pl = plugin;
     }
-
+    
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPluginEnable(PluginEnableEvent ev) {
         // testing for Heroes without importing it! :D
         Plugin pla = ev.getPlugin().getServer().getPluginManager().getPlugin("Heroes");
